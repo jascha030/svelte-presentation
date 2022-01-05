3a44d9996c9b698f691dd70b7cfefbd5abf6c22e
Jascha030 <contact@jaschavanaalst.nl>
2022-01-05 23:10:16 +0100

    🎨 Simplify logic in index.svelte

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,19 +1,17 @@
-<script lang='ts'>
-	import { getContext } from 'svelte';
-	import Layout from '../Layout';
+<script context="module" lang="ts">
 	import SlideContainer from '../components/SlideContainer.svelte';
 	import Slide from '../components/Slide.svelte';
+	import type SlideData from '../Typescript/SlideData';
+	import { getContext } from 'svelte';
+
+	const slides: SlideData[] = [
+		{title: 'test'},
+		{title: 'test2'},
+	];
 </script>
 
 <SlideContainer>
-	<Slide slideIndex={getContext('SetIndex').index()}
-				 title={'Jassie030'}
-				 layout={Layout.ImageLeft}
-				 img={'https://www.rd.com/wp-content/uploads/2021/04/GettyImages-1055061832-e1618333498370.jpg?w=1801'}>
-		<h2>Hamsterspreekbeurt</h2>
-	</Slide>
-	<Slide slideIndex={getContext('SetIndex').index()} title={'slide 2'} />
+	{#each slides as data, index}
+		<Slide slideData={Object.assign(data, {slideIndex: getContext('SetIndex').index()})} />
+	{/each}
 </SlideContainer>
-
-<style>
-</style>
