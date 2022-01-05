5d64f84138809763033d4a3ffffb156791b09282
Jascha030 <contact@jaschavanaalst.nl>
2022-01-05 23:07:46 +0100

    🚧 Change props to single prop of interface SlideData

---
+++ src/Typescript/SlideData.ts
@@ -0,0 +1,10 @@
+import type Layout from './Layout';
+
+interface SlideData {
+	title?: string,
+	layout?: Layout,
+	img?: string
+	slideIndex?: number
+}
+
+export default SlideData;
--- src/components/Slide.svelte
+++ src/components/Slide.svelte
@@ -1,71 +1,73 @@
 <script lang='ts'>
-	import Layout from '../Layout';
-	export let title = '';
-	export let layout = Layout.Center;
-	export let img = '';
-	export let slideIndex = 0;
+	import type SlideData from '../Typescript/SlideData';
+	import Layout from '../Typescript/Layout';
+
+	export let slideData: SlideData = {
+		title: '',
+		layout: Layout.Center,
+		img: '',
+		slideIndex: 0
+	};
 </script>
 
-<div class='slide {layout} {slideIndex === 0 ? "translate-x-0" : "translate-x-full" }'>
+<div class='slide {slideData.layout} {slideData.slideIndex === 0 ? "translate-x-0" : "translate-x-full" }'>
 	<div class='slide-content slide-text'>
-		<h1>{title}</h1>
-		<slot></slot>
+		<h1>{slideData.title}</h1>
 	</div>
 
-	{#if layout.includes('img')}
-		<div class='slide-content slide-img'>
-			<div class='slide-img-content' style="background-image: url({img})"></div>
-		</div>
-	{/if}
+	<!--{#if slideData.layout.includes('img')}-->
+	<!--	<div class='slide-content slide-img'>-->
+	<!--		<div class='slide-img-content' style='background-image: url({slideData.img})'></div>-->
+	<!--	</div>-->
+	<!--{/if}-->
 </div>
 
-<style lang='postcss'>
-    .slide {
-        @apply absolute inset-0 w-screen h-screen text-white flex transition-all ease-in-out duration-500 transform;
+<style lang='scss'>
+  .slide {
+    @apply absolute inset-0 w-screen h-screen text-white flex transition-all ease-in-out duration-500 transform;
+
+    &.center {
+      @apply flex justify-center text-left;
     }
 
-		.slide.center {
-				@apply flex justify-center;
-		}
-
-    .slide.img-left, .slide.img-right {
-        @apply grid grid-cols-2;
+    &.img-right, &.img-left, &.left, &.right {
+      @apply grid grid-cols-2;
     }
 
-		.slide-content {
-				@apply p-16;
-		}
+    .slide-content {
+      @apply p-16;
 
-		.slide-text {
-				z-index: 1;
-		}
+      &.slide-text {
+        z-index: 1;
+      }
 
-		.slide-img {
-				@apply pt-16 pb-20 flex;
+      &.slide-img {
+        @apply pt-16 pb-20 flex;
+        z-index: 0;
 
-				z-index:0;
-		}
+        .slide-img-content {
+          @apply transition ease-in-out grow bg-center bg-cover rounded-lg;
 
-		.slide-img-content {
-				@apply transition ease-in-out grow bg-center bg-cover rounded-lg;
-
-    		animation: bounce 50s infinite;
-    }
-
-    @keyframes bounce {
-        0%, 100% {
-						opacity: .9;
-            transform: translateY(-2%) translateX(4%);
-            animation-timing-function: cubic-bezier(0, 0.4, 2, 1);
+          animation: bounce 50s infinite;
         }
-        50% {
-						opacity: 1;
-            transform: translateY(0);
-            transition-timing-function: cubic-bezier(0, 0, 0.2, 1);
-        }
+      }
     }
 
     h1 {
-        @apply text-8xl font-sans font-bold drop-shadow-sm;
+      @apply text-8xl font-sans font-bold drop-shadow-sm;
     }
+  }
+
+  @keyframes bounce {
+    0%, 100% {
+      opacity: .9;
+      transform: translateY(-2%) translateX(4%);
+      animation-timing-function: cubic-bezier(0, 0.4, 2, 1);
+    }
+    50% {
+      opacity: 1;
+      transform: translateY(0);
+      transition-timing-function: cubic-bezier(0, 0, 0.2, 1);
+    }
+  }
 </style>
